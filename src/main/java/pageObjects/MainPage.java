3852b619cb3c7d3609b7903851416ec66969bea9
andrei.kuturmazov <andrei.kuturmazov@gmail.com>
2020-07-18 11:56:29 +0300

    allure-selenide branch adding

--- src/main/java/pageObjects/MainPage.java
+++ src/main/java/pageObjects/MainPage.java
@@ -1,36 +1,96 @@
 package pageObjects;
 
-import org.openqa.selenium.*;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.codeborne.selenide.Selenide.*;
+
 
 public class MainPage {
 
     public String baseUrl = "http://automationpractice.com/index.php";
-    static WebDriver driver;
+    List<String> productsToBuyNames;
 
-    private final String womenTabLink = "//div[@id='block_top_menu']/ul/li/a[contains(text(),'Women')]";
-    private final String dressesTabLink = "//div[@id='block_top_menu']/ul/li/a[contains(text(),'Dresses')]";
-    private final String tShirtsTabLink = "//div[@id='block_top_menu']/ul/li/a[contains(text(),'T-shirts')]";
+    private final String womenTabItem = "Blouse";
+    private final String dressesTabItem = "Printed Chiffon Dress";
+    private final String shirtsTabItem = "Faded Short Sleeve T-shirts";
+    private SelenideElement womenTabLink = $x("//div[@id='block_top_menu']/ul/li/a[contains(text(),'Women')]");
+    private SelenideElement dressesTabLink = $x("//div[@id='block_top_menu']/ul/li/a[contains(text(),'Dresses')]");
+    private SelenideElement tShirtsTabLink = $x("//div[@id='block_top_menu']/ul/li/a[contains(text(),'T-shirts')]");
+    private SelenideElement listButton = $x("//li[@id='list']//i");
+    private SelenideElement largeSize = $x("//*[@id='group_1']/option[@value='3']");
+    private SelenideElement middleSize = $x("//*[@id='group_1']/option[@value='2']");
+    private SelenideElement smallSize = $x("//*[@id='group_1']/option[@value='1']");
+    private SelenideElement addToCardButton = $x("//button[@name='Submit']");
+    private SelenideElement continueShoppingButton = $x("//div[@class='button-container']/span[@title='Continue shopping']");
+    private SelenideElement checkoutButton = $x("//div[@class='button-container']/a");
+    private ElementsCollection cartElements = $$x("//td/p[@class='product-name']");
+    private ElementsCollection deleteLink = $$x("//a[@class='cart_quantity_delete']");
+    private ElementsCollection itemsOnPage = $$x("//h5[@itemprop='name']");
 
-    public MainPage(WebDriver driver) {
-        this.driver = driver;
+    public void changeInteractionToList() {
+        listButton.click();
+    }
+
+    public List<String> getNamesForCartItems(ElementsCollection collection) {
+        productsToBuyNames = new ArrayList<>();
+        for (SelenideElement element: collection) {
+            productsToBuyNames.add(element.getText().trim());
+        }
+        return productsToBuyNames;
+    }
+
+    public void deleteRandomItemFromCart() throws InterruptedException {
+        getNamesForCartItems(cartElements);
+        int index = (int) (Math.random() * deleteLink.size());
+        deleteLink.get(index).click();
+    }
+
+    public String getOrderItemText(SelenideElement element) {
+        return element.getText();
+    }
+
+    public void addItemToCart(String item, SelenideElement dressSize, SelenideElement actionAfterSelection, ElementsCollection collection) {
+        changeInteractionToList();
+        for (SelenideElement element : collection) {
+            if (element.getText().trim().equalsIgnoreCase(item)) {
+                element.click();
+                break;
+            }
+        }
+        dressSize.click();
+        addToCardButton.click();
+        actionAfterSelection.click();
+    }
+
+    public void addItemFromWomenTab() {
+        addItemToCart(womenTabItem, smallSize, continueShoppingButton, itemsOnPage);
+    }
+
+    public void addItemFromDressesTab() {
+        addItemToCart(dressesTabItem, largeSize, continueShoppingButton, itemsOnPage);
+    }
+
+    public void addItemFromShirtsTab() {
+        addItemToCart(shirtsTabItem, middleSize, checkoutButton, itemsOnPage);
     }
 
     public void openMainPage() {
-        driver.get(baseUrl);
+        Selenide.open(baseUrl);
     }
 
     public void switchToWomenTab() {
-        openMainPage();
-        driver.findElement(By.xpath(womenTabLink)).click();
+        womenTabLink.click();
     }
 
     public void switchToDressesTab() {
-        openMainPage();
-        driver.findElement(By.xpath(dressesTabLink)).click();
+        dressesTabLink.click();
     }
 
     public void switchToShirtsTab() {
-        openMainPage();
-        driver.findElement(By.xpath(tShirtsTabLink)).click();
+        tShirtsTabLink.click();
     }
 }
--- src/test/java/ItemsOrderTest.java
+++ src/test/java/ItemsOrderTest.java
@@ -1,49 +1,19 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.WebDriver;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import pageObjects.MainPage;
-import pageObjects.OrderPage;
-
-import java.util.concurrent.TimeUnit;
-
 
 public class ItemsOrderTest {
 
-    public static WebDriver driver;
-
-    MainPage mainPage = new MainPage(driver);
-    OrderPage orderPage = new OrderPage(driver);
-
-    @BeforeAll
-    public static void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        ChromeOptions options = new ChromeOptions();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        options.setAcceptInsecureCerts(true);
-        options.addArguments("--disable-web-security",
-                "--user-data-dir",
-                "--allow-running-insecure-content");
-    }
-
-    @AfterAll
-    public static void tearDown() {
-        driver.quit();
-    }
+    MainPage mainPage = new MainPage();
 
     @Test
-    public void itemsOrderTest() {
+    public void itemsOrderTest() throws InterruptedException {
+        mainPage.openMainPage();
         mainPage.switchToWomenTab();
-        orderPage.addItemFromWomenTab();
+        mainPage.addItemFromWomenTab();
         mainPage.switchToDressesTab();
-        orderPage.addItemFromDressesTab();
+        mainPage.addItemFromDressesTab();
         mainPage.switchToShirtsTab();
-        orderPage.addItemFromShirtsTab();
-        orderPage.deleteRandomItemFromCart();
+        mainPage.addItemFromShirtsTab();
+        mainPage.deleteRandomItemFromCart();
     }
 }
