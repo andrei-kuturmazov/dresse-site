d114336a2fdaba4dd45a337e7ce0abe19a57d1b7
andrei.kuturmazov <andrei.kuturmazov@gmail.com>
2020-08-04 10:02:44 +0300

    final adding

--- src/main/java/pageObjects/MainPage.java
+++ src/main/java/pageObjects/MainPage.java
@@ -17,19 +17,19 @@ public class MainPage {
     private final String womenTabItem = "Blouse";
     private final String dressesTabItem = "Printed Chiffon Dress";
     private final String shirtsTabItem = "Faded Short Sleeve T-shirts";
-    private SelenideElement womenTabLink = $x("//div[@id='block_top_menu']/ul/li/a[contains(text(),'Women')]");
-    private SelenideElement dressesTabLink = $x("//div[@id='block_top_menu']/ul/li/a[contains(text(),'Dresses')]");
-    private SelenideElement tShirtsTabLink = $x("//div[@id='block_top_menu']/ul/li/a[contains(text(),'T-shirts')]");
-    private SelenideElement listButton = $x("//li[@id='list']//i");
-    private SelenideElement largeSize = $x("//*[@id='group_1']/option[@value='3']");
-    private SelenideElement middleSize = $x("//*[@id='group_1']/option[@value='2']");
-    private SelenideElement smallSize = $x("//*[@id='group_1']/option[@value='1']");
-    private SelenideElement addToCardButton = $x("//button[@name='Submit']");
-    private SelenideElement continueShoppingButton = $x("//div[@class='button-container']/span[@title='Continue shopping']");
-    private SelenideElement checkoutButton = $x("//div[@class='button-container']/a");
-    private ElementsCollection cartElements = $$x("//td/p[@class='product-name']");
-    private ElementsCollection deleteLink = $$x("//a[@class='cart_quantity_delete']");
-    private ElementsCollection itemsOnPage = $$x("//h5[@itemprop='name']");
+    private final SelenideElement womenTabLink = $x("//div[@id='block_top_menu']/ul/li/a[contains(text(),'Women')]");
+    private final SelenideElement dressesTabLink = $x("//div[@id='block_top_menu']/ul/li/a[contains(text(),'Dresses')]");
+    private final SelenideElement tShirtsTabLink = $x("//div[@id='block_top_menu']/ul/li/a[contains(text(),'T-shirts')]");
+    private final SelenideElement listButton = $x("//li[@id='list']//i");
+    private final SelenideElement largeSize = $x("//*[@id='group_1']/option[@value='3']");
+    private final SelenideElement middleSize = $x("//*[@id='group_1']/option[@value='2']");
+    private final SelenideElement smallSize = $x("//*[@id='group_1']/option[@value='1']");
+    private final SelenideElement addToCardButton = $x("//button[@name='Submit']");
+    private final SelenideElement continueShoppingButton = $x("//div[@class='button-container']/span[@title='Continue shopping']");
+    private final SelenideElement checkoutButton = $x("//div[@class='button-container']/a");
+    private final ElementsCollection cartElements = $$x("//td/p[@class='product-name']");
+    private final ElementsCollection deleteLink = $$x("//a[@class='cart_quantity_delete']");
+    private final ElementsCollection itemsOnPage = $$x("//h5[@itemprop='name']");
 
     public void changeInteractionToList() {
         listButton.click();
