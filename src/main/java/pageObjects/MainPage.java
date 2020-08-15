8b88c4b9e391021ff246ae0ff4e2fd367b463c00
andrei.kuturmazov <andrei.kuturmazov@gmail.com>
2020-08-15 15:16:26 +0300

    updating allure steps

--- src/main/java/pageObjects/MainPage.java
+++ src/main/java/pageObjects/MainPage.java
@@ -3,6 +3,8 @@ package pageObjects;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,24 +33,28 @@ public class MainPage {
     private final ElementsCollection deleteLink = $$x("//a[@class='cart_quantity_delete']");
     private final ElementsCollection itemsOnPage = $$x("//h5[@itemprop='name']");
 
+    @Step("Change items interaction to list")
     public void changeInteractionToList() {
         listButton.click();
     }
 
+    @Step("Get items on page names")
     public List<String> getNamesForCartItems(ElementsCollection collection) {
         productsToBuyNames = new ArrayList<>();
-        for (SelenideElement element: collection) {
+        for (SelenideElement element : collection) {
             productsToBuyNames.add(element.getText().trim());
         }
         return productsToBuyNames;
     }
 
+    @Step("Delete random item from cart")
     public void deleteRandomItemFromCart() {
         getNamesForCartItems(cartElements);
         int index = (int) (Math.random() * deleteLink.size());
         deleteLink.get(index).click();
     }
 
+    @Step("Add item to cart")
     public void addItemToCart(String item, SelenideElement dressSize, SelenideElement actionAfterSelection, ElementsCollection collection) {
         changeInteractionToList();
         for (SelenideElement element : collection) {
@@ -62,30 +68,37 @@ public class MainPage {
         actionAfterSelection.click();
     }
 
+    @Step("Select and add item from Women tab")
     public void addItemFromWomenTab() {
         addItemToCart(womenTabItem, smallSize, continueShoppingButton, itemsOnPage);
     }
 
+    @Step("Select and add item from Dresses tab")
     public void addItemFromDressesTab() {
         addItemToCart(dressesTabItem, largeSize, continueShoppingButton, itemsOnPage);
     }
 
+    @Step("Select and add item from Shirts tab")
     public void addItemFromShirtsTab() {
         addItemToCart(shirtsTabItem, middleSize, checkoutButton, itemsOnPage);
     }
 
+    @Step("Open site main page")
     public void openMainPage() {
         Selenide.open(baseUrl);
     }
 
+    @Step("Switch to Women Tab")
     public void switchToWomenTab() {
         womenTabLink.click();
     }
 
+    @Step("Switch to Dresses Tab")
     public void switchToDressesTab() {
         dressesTabLink.click();
     }
 
+    @Step("Switch to Shirts Tab")
     public void switchToShirtsTab() {
         tShirtsTabLink.click();
     }
--- src/test/java/tests/ItemsOrderTest.java
+++ src/test/java/tests/ItemsOrderTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.TestInstance;
 import pageObjects.MainPage;
 import utils.TestInit;
 
-import static io.qameta.allure.Allure.step;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ItemsOrderTest extends TestInit {
@@ -16,21 +15,13 @@ public class ItemsOrderTest extends TestInit {
     @Test
     @DisplayName("Добавление трех вещей в корзину и удаление случайной вещи")
     public void orderTest() {
-        step("Открыть главную страницу сайта", () ->
-            mainPage.openMainPage());
-        step("Перейти во вкладку \"Women\" и добавить вещь в корзину", () -> {
+            mainPage.openMainPage();
             mainPage.switchToWomenTab();
             mainPage.addItemFromWomenTab();
-        });
-        step("Перейти во вкладку \"Dresses\" и добавить вещь в корзину", () -> {
             mainPage.switchToDressesTab();
             mainPage.addItemFromDressesTab();
-        });
-        step("Перейти во вкладку \"Shirts\" и добавить вещь в корзину", () -> {
             mainPage.switchToShirtsTab();
             mainPage.addItemFromShirtsTab();
-        });
-        step("Удалить случайный товар из добавленных в корзину", () ->
-            mainPage.deleteRandomItemFromCart());
+            mainPage.deleteRandomItemFromCart();
     }
 }
